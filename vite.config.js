478322ba77999add48d5ee1a6a09da980cce8af1
Loïc Frémaux <55465703+loic-fremaux@users.noreply.github.com>
2021-05-02 17:45:38 +0200

    English translations

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import {resolve} from 'path'
 
-//on crée notre plugion pour refresh blade
+// Blade refresh plugin
 const bladeRefreshPlugin = {
     name: 'twig-refresh',
     configureServer ({watcher, ws}){
